6e77f2d79d4094c91f6c61d1fd146b59d68f1602
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-03 17:24:30 +0700

    belajar optionalChaining

---
+++ 1. javascriptFundamental/6optionalChaining.js
@@ -0,0 +1,7 @@
+// const user3 = null
+// console.log(user3?.name) // undefined
+// console.log(user3?.addrees?.street) // undefined
+
+// const user3 = null
+// console.log(user3?.name) // undefined
+// console.log(user3?.addrees?.city) // undefined
