0eabc42d6f62e5fe59b28e84fbd59793b28a113c
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-03 15:42:24 +0700

    belajar conditionalOperator

---
+++ 1. javascriptFundamental/5conditionalOperator.js
@@ -0,0 +1,21 @@
+// Ternary Operator
+// Short-circuit Evaluation
+
+// const nilai = 80
+// console.log((nilai >= 75) ? 'Lulus' : 'Tidak Lulus')
+
+// const user2 = {
+//   nama: 'Budi',
+//   umur: 27,
+//   pekerjaan: 'Programmer'
+// }
+// console.log(user2.pekerjaan || 'Pengangguran') // Programmer
+
+// const user = { name: 'Hasan' }
+// const Home = () => {
+//   return (
+//     <div>
+//       Selamat datang {user && user.name}!
+//     </div>
+//   )
+// }
