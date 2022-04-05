a0a27369cf9bb39ec6d0250610357dd1d4f761fb
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-05 10:51:42 +0700

    mengenal reactJS

---
+++ 2.pengenalanReactJS/1mengenalReactJS.js
@@ -0,0 +1,28 @@
+// Apa itu ReactJS?
+
+/* Terdapat tiga prinsip dasar dari pustaka ReactJS yang harus kita fahami. */
+/* 1. Component Based
+ReactJS menggunakan konsep komponen yaitu dengan memecah antar muka pengguna yang kompleks menjadi bagian-bagian kecil.
+Setiap bagian tersebut dapat berdiri sendiri dan dapat digunakan secara berulang. */
+/* 2. Declarative
+ReactJS akan memudahkan kita merancang antar muka pengguna yang interaktif,
+salah satunya dalam hal menampilkan & memperbaharui komponen dari aplikasi berdasarkan
+kondisi data tertentu secara efisien & terstruktur. */
+/* 3. Learn Once, Write Anywhere
+Tidak hanya cocok untuk membangun aplikasi berbasis web, namun ReactJS juga dapat digunakan untuk membangun
+aplikasi mobile native (iOS & Android) melalui pustaka ReactNative dan tentu saja dengan bantuan tools Javascript lain
+maka aplikasi ReactJS dapat di-compile menjadi aplikasi berbasis desktop.
+Nah, itu artinya dengan belajar ReactJS untuk aplikasi web, maka kemudian kita bisa gunakan pengetahuan tersebut
+untuk diterapkan ketika membangun aplikasi berbasis mobile & desktop. Ibarat pepatah
+"sekali mendayung, dua tiga pulau terlampaui". */
+/* Virtual DOM */
+
+// Sejarah Lahirnya ReactJS
+
+// Mengapa Memilih ReactJS?
+
+// ReactJS Cocok untuk Siapa?
+
+/* ReactJS sangat cocok bagi mereka yang mencintai Javascript! */
+/* Karena pustaka ini banyak mengekplorasi fitur-fitur dari Javascript secara mendalam.
+Yap, hampir setiap kodingan kita di ReactJS menggunakan sintaks Javascript. */
